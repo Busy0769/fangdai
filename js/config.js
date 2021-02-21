05f7d26797ae7b3d136ab2842b9521437ffcfcbe
刘学 <56247228@qq.com>
2021-02-21 18:08:37 +0800

    原利率上浮改成LPR版本
    2021年5年期LPR为4.65%

---
+++ js/config.js
@@ -0,0 +1,231 @@
+var config = {
+    loanyear: [{
+            year: 1,
+            select: false
+        },
+        {
+            year: 2,
+            select: false
+        },
+        {
+            year: 3,
+            select: false
+        },
+        {
+            year: 4,
+            select: false
+        },
+        {
+            year: 5,
+            select: false
+        },
+        {
+            year: 6,
+            select: false
+        },
+        {
+            year: 7,
+            select: false
+        },
+        {
+            year: 8,
+            select: false
+        },
+        {
+            year: 9,
+            select: false
+        },
+        {
+            year: 10,
+            select: false
+        },
+        {
+            year: 11,
+            select: false
+        },
+        {
+            year: 12,
+            select: false
+        },
+        {
+            year: 13,
+            select: false
+        },
+        {
+            year: 14,
+            select: false
+        },
+        {
+            year: 15,
+            select: false
+        },
+        {
+            year: 16,
+            select: false
+        },
+        {
+            year: 17,
+            select: false
+        },
+        {
+            year: 18,
+            select: false
+        },
+        {
+            year: 19,
+            select: false
+        },
+        {
+            year: 20,
+            select: false
+        },
+        {
+            year: 21,
+            select: false
+        },
+        {
+            year: 22,
+            select: false
+        },
+        {
+            year: 23,
+            select: false
+        },
+        {
+            year: 24,
+            select: false
+        },
+        {
+            year: 25,
+            select: false
+        },
+        {
+            year: 26,
+            select: false
+        },
+        {
+            year: 27,
+            select: false
+        },
+        {
+            year: 28,
+            select: false
+        },
+        {
+            year: 29,
+            select: false
+        },
+        {
+            year: 30,
+            select: true
+        }
+    ],
+    shangdaiLilv: [{
+            year: 1,
+            lilv: 0.0385
+        },
+        {
+            year: 6,
+            lilv: 0.0465
+        }
+    ],
+    shangdaiSelect: [{
+            lilv: 0,
+            name: "基准利率"
+        },
+        {lilv: 0.0005,name: "加5基点"},
+        {lilv: 0.001,name: "加10基点"},
+        {lilv: 0.0015,name: "加15基点"},
+        {lilv: 0.002,name: "加20基点"},
+        {lilv: 0.0025,name: "加25基点"},
+        {lilv: 0.003,name: "加30基点"},
+        {lilv: 0.0035,name: "加35基点"},
+        {lilv: 0.004,name: "加40基点"},
+        {lilv: 0.0045,name: "加45基点"},
+        {lilv: 0.005,name: "加50基点"},
+        {lilv: 0.0055,name: "加55基点"},
+        {lilv: 0.006,name: "加60基点"},
+        {lilv: 0.0065,name: "加65基点"},
+        {lilv: 0.007,name: "加70基点"},
+        {lilv: 0.0075,name: "加75基点"},
+        {lilv: 0.008,name: "加80基点"},
+        {lilv: 0.0085,name: "加85基点"},
+        {lilv: 0.009,name: "加90基点"},
+        {lilv: 0.0095,name: "加95基点"},
+        {lilv: 0.01,name: "加100基点"},
+        {lilv: -0.0005,name: "减5基点"},
+        {lilv: -0.001,name: "减10基点"},
+        {lilv: -0.0015,name: "减15基点"},
+        {lilv: -0.002,name: "减20基点"},
+        {lilv: -0.0025,name: "减25基点"},
+        {lilv: -0.003,name: "减30基点"},
+        {lilv: -0.0035,name: "减35基点"},
+        {lilv: -0.004,name: "减40基点"},
+        {lilv: -0.0045,name: "减45基点"},
+        {lilv: -0.005,name: "减50基点"},
+        {lilv: -0.0055,name: "减55基点"},
+        {lilv: -0.006,name: "减60基点"},
+        {lilv: -0.0065,name: "减65基点"},
+        {lilv: -0.007,name: "减70基点"},
+        {lilv: -0.0075,name: "减75基点"},
+        {lilv: -0.008,name: "减80基点"},
+        {lilv: -0.0085,name: "减85基点"},
+        {lilv: -0.009,name: "减90基点"},
+        {lilv: -0.0095,name: "减95基点"},
+        {lilv: -0.01,name: "减100基点"},
+    ],
+    gjjLilv: [{
+            year: 1,
+            lilv: 0.0275
+        },
+        {
+            year: 6,
+            lilv: 0.0325
+        }
+    ],
+    gjjSelect: [{
+            lilv: 0,
+            name: "基准利率"
+        },
+        {lilv: 0.0005,name: "加5基点"},
+        {lilv: 0.001,name: "加10基点"},
+        {lilv: 0.0015,name: "加15基点"},
+        {lilv: 0.002,name: "加20基点"},
+        {lilv: 0.0025,name: "加25基点"},
+        {lilv: 0.003,name: "加30基点"},
+        {lilv: 0.0035,name: "加35基点"},
+        {lilv: 0.004,name: "加40基点"},
+        {lilv: 0.0045,name: "加45基点"},
+        {lilv: 0.005,name: "加50基点"},
+        {lilv: 0.0055,name: "加55基点"},
+        {lilv: 0.006,name: "加60基点"},
+        {lilv: 0.0065,name: "加65基点"},
+        {lilv: 0.007,name: "加70基点"},
+        {lilv: 0.0075,name: "加75基点"},
+        {lilv: 0.008,name: "加80基点"},
+        {lilv: 0.0085,name: "加85基点"},
+        {lilv: 0.009,name: "加90基点"},
+        {lilv: 0.0095,name: "加95基点"},
+        {lilv: 0.01,name: "加100基点"},
+        {lilv: -0.0005,name: "减5基点"},
+        {lilv: -0.001,name: "减10基点"},
+        {lilv: -0.0015,name: "减15基点"},
+        {lilv: -0.002,name: "减20基点"},
+        {lilv: -0.0025,name: "减25基点"},
+        {lilv: -0.003,name: "减30基点"},
+        {lilv: -0.0035,name: "减35基点"},
+        {lilv: -0.004,name: "减40基点"},
+        {lilv: -0.0045,name: "减45基点"},
+        {lilv: -0.005,name: "减50基点"},
+        {lilv: -0.0055,name: "减55基点"},
+        {lilv: -0.006,name: "减60基点"},
+        {lilv: -0.0065,name: "减65基点"},
+        {lilv: -0.007,name: "减70基点"},
+        {lilv: -0.0075,name: "减75基点"},
+        {lilv: -0.008,name: "减80基点"},
+        {lilv: -0.0085,name: "减85基点"},
+        {lilv: -0.009,name: "减90基点"},
+        {lilv: -0.0095,name: "减95基点"},
+        {lilv: -0.01,name: "减100基点"},
+    ]
+}
